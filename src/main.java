d852b2d4c4638e6818c6d5f65f7e197e219a46fe
molbrich <molbrich@student.tgm.ac.at>
2017-02-20 11:17:53 +0100

    commit

---
+++ src/main.java
@@ -0,0 +1,15 @@
+
+public class main {
+
+	public static void hallo(){
+		System.out.println("molbrich lässt euch grüßen");
+	}
+	
+	
+	public static void main(String[] args) {
+		
+		hallo();
+		
+	}
+
+}
